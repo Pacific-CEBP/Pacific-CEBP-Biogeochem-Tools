07bdb1b43ed9d88186cf6d83bfac44e7a79d27f4
Paul Covert (DFO) <Paul.Covert2@dfo-mpo.gc.ca>
2021-05-26 14:37:33 -0700

    repaired module reference to correct_zero_order_hold

--- biogeochem/io.py
+++ biogeochem/io.py
@@ -9,6 +9,8 @@ import pandas as pd
 import xarray as xr
 import pyrsktools as rsk
 
+from . import ctd
+
 
 def load_event_log(fname):
     """Loads the event log .csv file.  Must contain specific columns
@@ -149,11 +151,11 @@ def import_merge_rbr(rsk_flist, expocode, root_dir=None, raw_dir=None,
     print('done.', flush=True)
 
     print('Correcting zero-order holds in raw traces...', end='', flush=True)
-    ds_raw = correct_zero_order_hold(ds_raw, 'P')
-    ds_raw = correct_zero_order_hold(ds_raw, 'T')
-    ds_raw = correct_zero_order_hold(ds_raw, 'C')
-    ds_raw = correct_zero_order_hold(ds_raw, 'V0')
-    ds_raw = correct_zero_order_hold(ds_raw, 'V1')
+    ds_raw = ctd.rbr_correct_zero_order_hold(ds_raw, 'P')
+    ds_raw = ctd.rbr_correct_zero_order_hold(ds_raw, 'T')
+    ds_raw = ctd.rbr_correct_zero_order_hold(ds_raw, 'C')
+    ds_raw = ctd.rbr_correct_zero_order_hold(ds_raw, 'V0')
+    ds_raw = ctd.rbr_correct_zero_order_hold(ds_raw, 'V1')
     print('done.', flush=True)
 
     print('Saving merged CTD data...', end='', flush=True)
